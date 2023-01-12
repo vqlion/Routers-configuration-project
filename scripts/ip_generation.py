44f9aa47c6609ec28eab9a4c1898549360d3f283
vql <valentinjossic@outlook.fr>
2023-01-12 09:29:41 +0100

    IP generation dumps a json file with the link's IPs

--- scripts/ip_generation.py
+++ scripts/ip_generation.py
@@ -1,9 +1,10 @@
 import re
 import load_json
+import json
 
-count, matrix = load_json.load('../intent_files/network_arc.json')
+count, matrix, arc = load_json.load('../intent_files/network_arc.json')
 
-list = []
+ip_list = []
 
 ip_range_input = '2002:100:1::'
 ip_mask_input = 48
@@ -18,9 +19,35 @@ if(re.match(ip_range_regex, ip_range_input)):
 ip_range_input = ip_range_input[:-1]
 
 #loops over the links and creates a new ip for each link, stores in a list
-#todo: actual creation of the link, affectation to each address to each link, and better support for more mask lengths
-for i in range(1, count):
+for i in range(1, count + 1):
     r=f'{i}'
-    list.append(f'{ip_range_input}{r}')
+    ip_list.append(f'{ip_range_input}{r}')
 
-print(list)
+#affects an ip to each link and stores it in a new json file based on architecture one
+for routers in arc['architecture']:
+    router_number = routers['router_number']
+    for neighbors in routers['neighbors']:
+        neighbor_number = neighbors['neighbor_number']
+
+        if not "link_IP" in neighbors: #if the link's ip is not set yet
+            if matrix[routers['router_number'] - 1][neighbors['neighbor_number'] - 1]: #check if it has been set before on another router
+                neighbors.update({"link_IP": ip_list.pop()}) #set it from the list if it hasn't
+                matrix[routers['router_number'] - 1][neighbors['neighbor_number'] - 1] = 0
+                matrix[neighbors['neighbor_number'] - 1][routers['router_number'] - 1] = 0 #update the matrix to indicate this link has an ip
+            else: #if it has been set on another router
+                symetric_router = next(item for item in arc['architecture'] if item['router_number'] == neighbor_number) #get the corresponding router
+                symetric_link = next(item for item in symetric_router['neighbors'] if item['neighbor_number'] == router_number) #get the actual link
+                ip = symetric_link['link_IP'] #get the IP of the link
+                neighbors.update({"link_IP": ip}) #set the IP on this router
+
+for i in arc['architecture']:
+    print(f'For router {i["router_number"]}')
+    for j in i['neighbors']:
+        print(f'{j}')
+
+with open('test_out.json', 'x') as json_file:
+    json.dump(arc, json_file, 
+                        indent=4,  
+                        separators=(',',': ')) #save in a new json file
+
+print(ip_list)
--- scripts/load_json.py
+++ scripts/load_json.py
@@ -9,23 +9,24 @@ def load(file_path):
 
     arc = json.load(f)
 
-    for i in arc['architecture']:
-        print(f'For router {i["router_number"]}')
-        for j in i['neighbors']:
-            print(f'{j}')
+    # for i in arc['architecture']:
+    #     print(f'For router {i["router_number"]}')
+    #     for j in i['neighbors']:
+    #         print(f'{j}')
 
     router_count = len(arc['architecture'])
     adjacency_matrix = [[0 for _ in range(router_count)] for _ in range(router_count)]
     link_count = 0
 
-    for router in arc['architecture']:
-        for neighbors in router['neighbors']:
-            adjacency_matrix[router['router_number'] - 1][neighbors["neighbor_number"] - 1] = 1
+    for routers in arc['architecture']:
+        for neighbors in routers['neighbors']:
+            adjacency_matrix[routers['router_number'] - 1][neighbors["neighbor_number"] - 1] = link_count + 1
             link_count += 1
 
     link_count /= 2
 
-    # print("Adj", adjacency_matrix)
+    print("Adj", adjacency_matrix)
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in adjacency_matrix]))
     # print("count", link_count)
-    return int(link_count) , adjacency_matrix
+    return int(link_count) , adjacency_matrix, arc
 
