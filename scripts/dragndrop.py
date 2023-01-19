99c5f85cc6c7b413a0bdf3518335a28d564c1f2b
Alex Baciu <baciu6657@gmail.com>
2023-01-19 11:26:30 +0200

    DragNDropBot

---
+++ scripts/dragndrop.py
@@ -0,0 +1,20 @@
+import ip_generation as gen
+import json
+import os
+import sys
+import shutil
+
+
+
+
+with open('/mnt/c/INSALyonTC/GNS3_Project_3TC/intent_files/dragndrop.json', 'r') as file:
+    architecture = json.load(file)
+
+    for router in architecture['architecture']:
+        router_number=router['router_number']
+        removed_file=router['removed_file']
+        source_file=router['source_file']
+        os.remove(removed_file)
+        shutil.copyfile(source_file, removed_file)
+
+        
