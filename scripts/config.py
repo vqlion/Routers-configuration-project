bfccaad1714fb407432f4466a53e334ec29d7a22
rebeccadjim <117664113+rebeccadjim@users.noreply.github.com>
2023-01-26 10:52:04 +0100

    As_path_prepend

--- scripts/config.py
+++ scripts/config.py
@@ -234,8 +234,22 @@ def generate_BGP_policies(router_intents):
         BGP_configuration += f'router bgp {AS_NUMBER}\n'
         BGP_configuration += 'address-family ipv6\n'
         BGP_configuration += f' neighbor {neighbor_IP_address} route-map map_in_{count} in\n'
+        if "eBGP_config" in router_intents:
+            BGP_configuration += f' neighbor {neighbor_IP_address} route-map map_out_{count} out\n'
         BGP_configuration += f' neighbor {neighbor_IP_address} route-map map_out_{count} out\n' if len(communities_out) > 0 else ''
         BGP_configuration += 'exit-address-family\n!\n'
+
+         #AS-Path prepending configuration 
+        if "eBGP_config" in router_intents:
+            BGP_configuration += f"route-map map_out_{count} permit 5\n"
+            eBGP_config=router_intents["eBGP_config"]
+            for config in eBGP_config:
+                if "AS_path_prepend" in config:
+                    BGP_configuration += f'set as-path prepend '
+                    x = config["AS_path_prepend"]
+                    for i in range(x):
+                        BGP_configuration+= f'{AS_NUMBER} '
+                    BGP_configuration += '\n'
 
         count +=1
 
